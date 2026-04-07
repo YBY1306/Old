fd55cb55ff0c1a0d398a036252faa57fc36a65ac
拨开乌云便有阳光 <YBY1306>
2026-04-07 12:27:00 +0800

    .

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google ()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // 作为 Xposed 模块使用务必添加，其它情况可选
+        maven("https://api.xposed.info/")
+    }
+}
+
+rootProject.name = "WechatNotificationMessagingStyle"
+include(":app")
+ 
